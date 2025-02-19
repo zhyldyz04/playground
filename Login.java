28c80b4cc9f382333eb54e514e5198df34d8b396
zhyldyz04 <127899645+zhyldyz04@users.noreply.github.com>
2025-02-18 19:13:05 -0600

    selfPracticing fetch

--- Login.java
+++ Login.java
@@ -1,7 +1,9 @@
 public class Login{
 public static void  main (String [] args){
 System.out.println("Log in to Hello World ");
-  SYstem.out.println("Practicing fetch");
+  System.out.println("Practicing fetch");
+
+  System.out.println("Adding more fetch");
 
 
 
