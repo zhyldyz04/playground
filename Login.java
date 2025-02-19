dc26033d90b7c5487b41188ead72b189db721842
zhyldyz04 <maratbekova04@gmail.com>
2025-02-18 19:48:54 -0600

    resolved a conflict after revert

--- Login.java
+++ Login.java
@@ -1,9 +1,9 @@
 public class Login{
 public static void  main (String [] args){
 System.out.println("Log in to Hello World ");
-  System.out.println("Practicing fetch");
 
-  System.out.println("Adding more fetch");
+
+  int a = 5;
 
 
 
